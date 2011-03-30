725250d89e3644bcff2b04272aca2c90a317051d
Ashley McKenzie <ash@ashmckenzie.org>
2011-03-31 07:52:35 +1100

    added rtrim

--- lib/string.rb
+++ lib/string.rb
@@ -9,4 +9,13 @@ class String
     self.gsub(/#{r}/, '')
   end
 
+  # Trim characters from the end of string (defaults to spaces)
+  #
+  def rtrim(chars)
+    chars = "\s" if chars.nil?
+    chars = Regexp.escape(chars)
+    r = "#{chars}*$"
+    self.gsub(/#{r}/, '')
+  end
+
 end
--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -4,13 +4,21 @@ require File.dirname(__FILE__) + '/../lib/string'
 
 describe String do
 
-  before :each do
-    @strings = [
+  before :all do
+    @trim_strings = [
       [ ' some test string ', nil, 'some test string' ],
       [ '/test', '/', 'test' ],
       [ 'test/', '/', 'test' ],
       [ '/test/', '/', 'test' ],
       [ '//test//', '/', 'test' ],
+    ]
+
+    @rtrim_strings = [
+      [ ' some test string ', nil, ' some test string' ],
+      [ '/test', '/', '/test' ],
+      [ 'test/', '/', 'test' ],
+      [ '/test/', '/', '/test' ],
+      [ '//test//', '/', '//test' ],
     ]
   end
 
@@ -19,13 +27,16 @@ describe String do
     s.should respond_to(:trim)
   end
 
-  it "should trim the correct characters from given strings" do
-    @strings.each do |s|
+  it "should trim the correct characters from given trim strings" do
+    @trim_strings.each do |s|
       s[0].trim(s[1]).should eql(s[2])
     end
   end
 
-  after :each do
+  it "should trim the correct characters from given rtrim strings" do
+    @rtrim_strings.each do |s|
+      s[0].rtrim(s[1]).should eql(s[2])
+    end
   end
 
 end
