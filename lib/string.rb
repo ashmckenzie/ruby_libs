e2ff55285f2aa32aeecf03000c46ae8eeddbba13
Ashley McKenzie <ash@ashmckenzie.org>
2011-03-31 18:07:10 +1100

    added ltrim, refactored and fixed tests

--- lib/string.rb
+++ lib/string.rb
@@ -1,21 +1,29 @@
 class String
 
-  # Trim characters from the start and end of string (defaults to spaces)
+  # Trim characters from the start and end of string
   #
   def trim(chars)
-    chars = "\s" if chars.nil?
-    chars = Regexp.escape(chars)
-    r = "^#{chars}*|#{chars}*$"
-    self.gsub(/#{r}/, '')
+    _trim("^%s*|%s*$", chars)
   end
 
-  # Trim characters from the end of string (defaults to spaces)
+  # Trim characters from the start of string
+  #
+  def ltrim(chars)
+    _trim("^%s*", chars)
+  end
+
+  # Trim characters from the end of string
   #
   def rtrim(chars)
-    chars = "\s" if chars.nil?
-    chars = Regexp.escape(chars)
-    r = "#{chars}*$"
-    self.gsub(/#{r}/, '')
+    _trim("%s*$", chars)
   end
 
+  private
+
+  def _trim(regex, chars)
+    chars = '\s' if chars.nil?
+    chars = Regexp.escape(chars)
+    regex.gsub!(/%s/, chars)
+    self.gsub(/#{regex}/, '')
+  end
 end
--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -5,12 +5,21 @@ require File.dirname(__FILE__) + '/../lib/string'
 describe String do
 
   before :all do
+
     @trim_strings = [
       [ ' some test string ', nil, 'some test string' ],
       [ '/test', '/', 'test' ],
       [ 'test/', '/', 'test' ],
       [ '/test/', '/', 'test' ],
       [ '//test//', '/', 'test' ],
+    ]
+
+    @ltrim_strings = [
+      [ ' some test string ', nil, 'some test string ' ],
+      [ '/test', '/', 'test' ],
+      [ 'test/', '/', 'test/' ],
+      [ '/test/', '/', 'test/' ],
+      [ '//test//', '/', 'test//' ],
     ]
 
     @rtrim_strings = [
@@ -20,6 +29,7 @@ describe String do
       [ '/test/', '/', '/test' ],
       [ '//test//', '/', '//test' ],
     ]
+
   end
 
   it "should respond to the trim message" do
@@ -27,13 +37,19 @@ describe String do
     s.should respond_to(:trim)
   end
 
-  it "should trim the correct characters from given trim strings" do
+  it "should trim the correct characters from given trim array" do
     @trim_strings.each do |s|
       s[0].trim(s[1]).should eql(s[2])
     end
   end
 
-  it "should trim the correct characters from given rtrim strings" do
+  it "should trim the correct characters from given ltrim array" do
+    @ltrim_strings.each do |s|
+      s[0].ltrim(s[1]).should eql(s[2])
+    end
+  end
+
+  it "should trim the correct characters from given rtrim array" do
     @rtrim_strings.each do |s|
       s[0].rtrim(s[1]).should eql(s[2])
     end
